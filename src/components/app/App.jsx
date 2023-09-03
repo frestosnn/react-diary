1c9d88c6d1f15eba55cee4c14456b805fa9cf9b2
Комарова Анастасия <frestos.n@yandex.ru>
2023-09-04 00:46:25 +0300

    исправление ошибок

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -7,20 +7,24 @@ import Filter from '../filter/Filter';
 import PopupPost from '../popupPost/PopupPost';
 
 function App() {
+  //посты из хранилища
   const storedPosts = localStorage.getItem('userPosts');
   const initialPosts = storedPosts ? JSON.parse(storedPosts) : [];
 
+  //стейты
   const [posts, setPosts] = useState(initialPosts);
-
   const [isOpen, setOpen] = useState(false);
   const [isPopupPostOpen, setPopupPostOpen] = useState(false);
-
   const [filter, setFilter] = useState({ sort: '', query: '' });
-
   const [selectedPost, setSelectedPost] = useState({});
-
   const [shouldUpdatePosts, setShouldUpdatePosts] = useState(false);
 
+  //функция добавления данных в Local Storage
+  function updateLocalStorage(updatedPosts) {
+    localStorage.setItem('userPosts', JSON.stringify(updatedPosts));
+  }
+
+  //сотрировка постов
   const sortedPosts = useMemo(() => {
     if (filter.sort) {
       return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
@@ -28,55 +32,62 @@ function App() {
     return posts; // Вернуть исходный массив, если сортировка не выбрана
   }, [filter.sort, posts]);
 
+  //поиск постов
   const sortedAndSearchedPosts = useMemo(() => {
     return sortedPosts.filter(
       post => post && post.title && post.title.toLowerCase().includes(filter.query)
     );
   }, [filter.query, sortedPosts]);
 
+  //функция открытия попапа
   function handlePopupOpen() {
     setOpen(true);
   }
 
+  //функция открытия попапа-постов
   function handlePopupPostOpen() {
     setPopupPostOpen(true);
   }
 
+  //функция закрытия попапов
   function closePopup() {
     setOpen(false);
     setPopupPostOpen(false);
     setSelectedPost({});
   }
 
+  //добавление нового поста
   function createPost(newPost) {
-    const updatedPosts = [...posts, newPost];
+    const updatedPosts = [...sortedAndSearchedPosts, newPost];
     setPosts(updatedPosts);
 
     // Обновление данных в LocalStorage
-    localStorage.setItem('userPosts', JSON.stringify(updatedPosts));
+    updateLocalStorage(updatedPosts);
   }
 
+  //функция удаления поста
   function removePost(card) {
-    const updatedPosts = posts.filter(p => p.id !== card.id);
+    const updatedPosts = sortedAndSearchedPosts.filter(p => p.id !== card.id);
     setPosts(updatedPosts);
 
     // Обновление данных в LocalStorage
-    localStorage.setItem('userPosts', JSON.stringify(updatedPosts));
+    updateLocalStorage(updatedPosts);
   }
 
+  //функция редактирования поста
   function updatePost(updatedPost) {
-    const updatedPosts = posts.map(post => {
+    const updatedPosts = sortedAndSearchedPosts.map(post => {
       if (post && post.id === updatedPost.id) {
-        return updatedPost; // Обновляем существующий пост
+        return updatedPost; // обновляем существующий пост
       }
-      return post; // Остальные посты оставляем без изменений
+      return post; // остальные посты оставляем без изменений
     });
 
     setSelectedPost(updatedPost);
     setPosts(updatedPosts);
 
     // Обновление данных в LocalStorage
-    localStorage.setItem('userPosts', JSON.stringify(updatedPosts));
+    updateLocalStorage(updatedPosts);
   }
 
   return (
